618c2efaab5f9f32b3448e88a349b8ce560f3e5b
balaji <gbaalaji27@gmail.com>
2024-03-17 15:32:42 +0530

    coded get single product API

---
+++ src/main/java/com/scaler/productservicedecmwfeve/configs/ApplicationConfiguration.java
@@ -0,0 +1,15 @@
+package com.scaler.productservicedecmwfeve.configs;
+
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.client.RestTemplate;
+
+@Controller
+public class ApplicationConfiguration {
+    @Bean
+    public RestTemplate createRestTemplate(){
+        return new RestTemplateBuilder().build();
+    }
+}
--- src/main/java/com/scaler/productservicedecmwfeve/controller/ProductController.java
+++ src/main/java/com/scaler/productservicedecmwfeve/controller/ProductController.java
@@ -1,7 +1,10 @@
 package com.scaler.productservicedecmwfeve.controller;
 
 import com.scaler.productservicedecmwfeve.models.Product;
+import com.scaler.productservicedecmwfeve.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -9,14 +12,22 @@ import java.util.List;
 @RestController
 @RequestMapping("/products")
 public class ProductController {
+
+    private ProductService productService;
+    @Autowired
+    public ProductController(ProductService productService){
+        this.productService = productService;
+    }
+
     @GetMapping() //localhost:8080/products
     public List<Product> getAllProducts(){
+//        restTemplate.delete(null);
         return new ArrayList<>();
     }
 
     @GetMapping("/{id}")
     public Product getSingleProduct(@PathVariable("id") Long id){
-        return new Product();
+        return productService.getSingleProduct(id);
     }
 
     @PostMapping()
